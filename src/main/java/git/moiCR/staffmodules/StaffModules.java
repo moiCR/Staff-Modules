a0530fa3f8e64a4e34aedcc534b5c58582f11c0a
moiCR <moisesmarenco13@gmail.com>
2026-01-02 13:13:35 -0600

    Initial Upload

---
+++ src/main/java/git/moiCR/staffmodules/StaffModules.java
@@ -0,0 +1,16 @@
+package git.moiCR.staffmodules;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffModules extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+          
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
